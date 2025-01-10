1efe331cfdc6a0c598e809af045934a60f4c5e4c
Ghada-Ataaoui <lataoui.ghada@gmail.com>
2025-01-10 13:42:25 +0100

    changed some configs

--- vite.config.js
+++ vite.config.js
@@ -1,10 +1,12 @@
 import { defineConfig } from 'vite';
 
 export default defineConfig({
-  base: './', // For custom domain
+  root: '.', // Set the root directory for the project
   build: {
+    outDir: 'dist', // Specify the output directory
+    emptyOutDir: true, // Clear the output directory before building
     rollupOptions: {
-      input: './main.js',
+      input: './index.html', // Ensure Vite processes the correct entry point
     },
   },
 });
